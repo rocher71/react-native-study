6c0aab04317c4d551674be37224705431200b288
Yejin Kim <qja086351@naver.com>
2022-05-04 00:20:58 +0900

    #2.1 Weather App - The Rules of Native
    StyleShhet.create({}), Css difference, <StatusBar/>

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>sdkfjakfjsalfjl</Text>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
   },
 });
