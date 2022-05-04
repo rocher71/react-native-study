2fdff4d048a2ba873315e88f4546e273157ae93f
Yejin Kim <qja086351@naver.com>
2022-05-04 13:02:33 +0900

    #2.7 Weather App - Location
    rn의 Dimensions api 사용 - 디바이스의 크기
    Expo의 location api 사용 - 위치 사용 허용, 위치 가져오기, city가져오기

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,15 +1,37 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import * as Location from "expo-location";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import { useEffect, useState } from "react";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
