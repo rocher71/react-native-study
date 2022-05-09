478d29a8363cf0c39889653af0ee85f241930934
Yejin Kim <qja086351@naver.com>
2022-05-09 16:08:17 +0900

    수정 시 input창에 현재값 들어가도록 함.

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
   Text,
@@ -8,62 +8,63 @@ import {
   ScrollView,
   Alert,
   Platform,
-} from 'react-native'
-import { useState, useEffect } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+} from "react-native";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { Fontisto, MaterialIcons } from '@expo/vector-icons'
-import { theme } from './colors'
+import { Fontisto, MaterialIcons } from "@expo/vector-icons";
+import { theme } from "./colors";
 
-const STORAGE_KEY = '@toDos'
-const STORAGE_KEY_WORKING = '@working'
+const STORAGE_KEY = "@toDos";
+const STORAGE_KEY_WORKING = "@working";
 
 export default function App() {
-  const [working, setWorking] = useState(true)
-  const [edit, setEdit] = useState(false)
-  const [text, setText] = useState('')
-  const [toDos, setToDos] = useState({}) //not an array, but hashmap!
+  const [working, setWorking] = useState(true);
+  const [edit, setEdit] = useState(false);
+  const [text, setText] = useState("");
+  const [editText, setEditText] = useState("");
+  const [toDos, setToDos] = useState({}); //not an array, but hashmap!
   useEffect(() => {
-    loadWorking()
-    loadToDos()
+    loadWorking();
+    loadToDos();
     //console.log(toDos);
-  }, [])
+  }, []);
   const travel = () => {
-    setWorking(false)
-    saveWorking(false)
-  }
+    setWorking(false);
+    saveWorking(false);
+  };
   const work = () => {
-    setWorking(true)
-    saveWorking(true)
-  }
-  const onChangeText = (payload) => setText(payload)
+    setWorking(true);
+    saveWorking(true);
+  };
+  const onChangeText = (payload) => setText(payload);
 
   const saveWorking = async (isWorking) => {
-    await AsyncStorage.setItem(STORAGE_KEY_WORKING, JSON.stringify(isWorking))
-  }
+    await AsyncStorage.setItem(STORAGE_KEY_WORKING, JSON.stringify(isWorking));
+  };
   const loadWorking = async () => {
-    const loadedWorking = await AsyncStorage.getItem(STORAGE_KEY_WORKING)
+    const loadedWorking = await AsyncStorage.getItem(STORAGE_KEY_WORKING);
     //console.log("loadedWorking : ", loadedWorking);
     if (loadedWorking) {
-      setWorking(JSON.parse(loadedWorking))
+      setWorking(JSON.parse(loadedWorking));
     }
-  }
+  };
 
   const saveToDos = async (toSave) => {
     //console.log("todos in savetodos: ", toSave);
     //take todos, turn into string, save using asyncStorage
-    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
-  }
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
   const loadToDos = async () => {
-    const s = await AsyncStorage.getItem(STORAGE_KEY)
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
     if (s) {
-      setToDos(JSON.parse(s))
+      setToDos(JSON.parse(s));
     }
-  }
+  };
 
   const addToDo = async () => {
-    if (text === '') {
-      return
+    if (text === "") {
+      return;
     }
     //save to do
     // const newToDos = Object.assign({}, toDos, {
@@ -72,67 +73,70 @@ export default function App() {
     const newToDos = {
       ...toDos,
       [Date.now()]: { text, working, checked: false, edit: false },
-    }
-    setToDos(newToDos)
-    await saveToDos(newToDos)
-    setText('')
-  }
+    };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
   const deleteToDo = (key) => {
-    if (Platform.OS === 'web') {
-      const ok = confirm('Do you want to delete this To Do?')
+    if (Platform.OS === "web") {
+      const ok = confirm("Do you want to delete this To Do?");
       if (ok) {
-        const newToDos = { ...toDos }
-        delete newToDos[key]
-        setToDos(newToDos) //update the state
-        saveToDos(newToDos) //save in the asyncStorage
+        const newToDos = { ...toDos };
+        delete newToDos[key];
+        setToDos(newToDos); //update the state
+        saveToDos(newToDos); //save in the asyncStorage
       }
-      return
+      return;
     }
-    Alert.alert('Delete To Do', 'Are you sure?', [
-      { text: 'Cancel' },
+    Alert.alert("Delete To Do", "Are you sure?", [
+      { text: "Cancel" },
       {
         text: "I'm Sure",
-        style: 'destructive',
+        style: "destructive",
         onPress: () => {
           //create a new toDos that 'without' the todo that has the parameter key
-          const newToDos = { ...toDos }
-          delete newToDos[key]
-          setToDos(newToDos) //update the state
-          saveToDos(newToDos) //save in the asyncStorage
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos); //update the state
+          saveToDos(newToDos); //save in the asyncStorage
         },
       },
-    ])
-  }
+    ]);
+  };
   const checkToDo = (key) => {
-    const newToDos = { ...toDos }
-    newToDos[key].checked = !newToDos[key].checked
-    setToDos(newToDos)
-    saveToDos(newToDos)
-  }
+    const newToDos = { ...toDos };
+    newToDos[key].checked = !newToDos[key].checked;
+    setToDos(newToDos);
+    saveToDos(newToDos);
+  };
   const goEditMode = (key) => {
     if (edit) {
-      Alert.alert('Edit one at a time!', '', [{ text: 'OK' }])
-      return
+      Alert.alert("Edit one at a time!", "", [{ text: "OK" }]);
+      return;
     }
-    setEdit(true)
-    const newToDos = { ...toDos }
-    newToDos[key].edit = true
-    setToDos(newToDos)
-  }
+    const newToDos = { ...toDos };
+    setEdit(true);
+    setEditText(newToDos[key].text);
+    newToDos[key].edit = true;
+    setToDos(newToDos);
+  };
+  const onEditTodo = (payload) => setEditText(payload);
   const editToDo = (event, key) => {
     //console.log(event.nativeEvent, key);
-    const editedText = event.nativeEvent.text
-    if (event.text === '') {
-      return
+    const editedText = event.nativeEvent.text;
+    if (event.text === "") {
+      return;
     }
-    const newToDos = { ...toDos }
-    newToDos[key].text = editedText
-    newToDos[key].edit = false
+    const newToDos = { ...toDos };
+    newToDos[key].text = editedText;
+    newToDos[key].edit = false;
     //console.log(newToDos[key]);
-    setToDos(newToDos)
-    saveToDos(newToDos)
-    setEdit(false)
-  }
+    setToDos(newToDos);
+    saveToDos(newToDos);
+    setEdit(false);
+    setEditText("");
+  };
 
   return (
     <View style={styles.container}>
@@ -140,7 +144,7 @@ export default function App() {
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
           <Text
-            style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
           >
             Work
           </Text>
@@ -149,7 +153,7 @@ export default function App() {
           <Text
             style={{
               ...styles.btnText,
-              color: !working ? 'white' : theme.grey,
+              color: !working ? "white" : theme.grey,
             }}
           >
             Travel
@@ -162,7 +166,7 @@ export default function App() {
         onChangeText={onChangeText}
         value={text}
         returnKeyType="done"
-        placeholder={working ? 'Add a To Do!' : 'Where do you want to go?'}
+        placeholder={working ? "Add a To Do!" : "Where do you want to go?"}
         style={styles.input}
       ></TextInput>
       <ScrollView>
@@ -182,6 +186,10 @@ export default function App() {
                           returnKeyType="done"
                           style={styles.toDoText}
                           onSubmitEditing={(e) => editToDo(e, key)}
+                          value={editText}
+                          onChangeText={onEditTodo}
+                          autoCapitalize="none"
+                          autoCorrect={false}
                           autoFocus
                         ></TextInput>
                       </View>
@@ -224,7 +232,7 @@ export default function App() {
           )}
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -234,17 +242,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   header: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 100,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   btnText: {
     fontSize: 38,
-    fontWeight: '600',
-    color: 'white',
+    fontWeight: "600",
+    color: "white",
   },
   input: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
@@ -256,26 +264,26 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: theme.toDoBg,
     borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   toDoText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   checkedToDoText: {
     color: theme.lightGrey,
     fontSize: 16,
-    fontWeight: '500',
-    textDecorationLine: 'line-through',
+    fontWeight: "500",
+    textDecorationLine: "line-through",
   },
   todoIcons: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   todoIcon: {
     marginHorizontal: 5,
     color: theme.grey,
   },
-})
+});
