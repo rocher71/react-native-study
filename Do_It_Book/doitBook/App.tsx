05d8168471f0f61e531449b39057542055134d28
Yejin Kim <qja086351@naver.com>
2022-05-20 15:42:16 +0900

    #2-1. 리액트 네이티브 프레임워크의 작동 원리

--- Do_It_Book/doitBook/App.tsx
+++ Do_It_Book/doitBook/App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const textElement = React.createElement(Text, null, "Hello world!");
+  return textElement;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
